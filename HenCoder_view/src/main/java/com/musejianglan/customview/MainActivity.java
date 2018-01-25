780272a83500976813f6a83f7b73967f448257e7
musejianglan <musejianglan@163.com>
2018-01-25 14:13:23 +0800

    hencoder自定义view学习 动画2

--- HenCoder_view/src/main/java/com/musejianglan/customview/MainActivity.java
+++ HenCoder_view/src/main/java/com/musejianglan/customview/MainActivity.java
@@ -11,6 +11,7 @@ import com.hencoder.hencoderpracticedraw3.DrawTextActivity;
 import com.hencoder.hencoderpracticedraw4.CanvasClipMatrixActivity;
 import com.hencoder.hencoderpracticedraw5.View1_5Activity;
 import com.hencoder.hencoderpracticedraw6.Animator1Activity;
+import com.hencoder.hencoderpracticedraw7.Animator2Activity;
 import com.musejianglan.customview.view1_1_base.BaseViewActivity;
 import com.musejianglan.customview.view1_2_paint_detail.PaintDetailActivity;
 
@@ -18,10 +19,11 @@ public class MainActivity extends ListActivity {
 
     private String[] names = {"自定义 View 1-1 绘制基础","自定义 View 1-2 Paint 详解",
             "自定义 View 1-3 drawText() 文字的绘制","自定义 View 1-4 Canvas 对绘制的辅助 clipXXX() 和 Matrix",
-            "自定义 View 1-5 绘制顺序","自定义 View 1-6：属性动画 Property Animation（上手篇）"};
+            "自定义 View 1-5 绘制顺序","自定义 View 1-6：属性动画 Property Animation（上手篇）",
+    "自定义 View 1-7：属性动画 Property Animation（进阶篇）"};
     private Class[] activities = {BaseViewActivity.class, PaintDetailActivity.class,
             DrawTextActivity.class,CanvasClipMatrixActivity.class, View1_5Activity.class,
-            Animator1Activity.class};
+            Animator1Activity.class, Animator2Activity.class};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
