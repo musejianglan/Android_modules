9ca35fcd2484eafc291a224d8972dac079e92950
liulei <musejianglan@163.com>
2018-03-10 09:58:11 +0800

    android permission

---
+++ droidpermission/src/main/java/com/promise/droidpermission/MainActivity.java
@@ -0,0 +1,149 @@
+package com.promise.droidpermission;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+/*        <uses-permission android:name="android.permission.WRITE_CALENDAR"/>
+    <uses-permission android:name="android.permission.CAMERA"/>
+    <uses-permission android:name="android.permission.WRITE_CONTACTS"/>
+    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+    <uses-permission android:name="android.permission.RECORD_AUDIO"/>
+    <uses-permission android:name="android.permission.READ_PHONE_STATE"/>
+    <uses-permission android:name="android.permission.BODY_SENSORS"/>
+    <uses-permission android:name="android.permission.RECEIVE_SMS"/>
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>*/
+    // 所需的全部权限
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.WRITE_CALENDAR,
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.BODY_SENSORS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+
+            Manifest.permission.SYSTEM_ALERT_WINDOW,
+
+    };
+
+    private static final String PACKAGE_URL_SCHEME = "package:"; // 方案
+
+    List<String> mPermissionList = new ArrayList<>();
+
+    private static final int PERMISSION_REQUEST_CODE = 0; // 系统权限管理页面的参数
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void requestPermissions(View view) {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mPermissionList.clear();
+            //检查是否已获取权限
+            for (int i = 0; i < PERMISSIONS.length; i++) {
+                if (ContextCompat.checkSelfPermission(this, PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
+                    mPermissionList.add(PERMISSIONS[i]);
+                }
+            }
+
+            if (!mPermissionList.isEmpty()) {//获取权限
+                String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);//将List转为数组
+                ActivityCompat.requestPermissions(MainActivity.this, permissions, PERMISSION_REQUEST_CODE);
+            } else {
+                toast("所需权限已全部获取");
+            }
+
+        }else {
+
+            toast("Android版本小于23，获取动态获取权限");
+
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (int i = 0; i < grantResults.length; i++) {
+
+                int grantResult = grantResults[i];
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    boolean showRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permissions[i]);
+                    if (showRequestPermission) {//
+                        judgePermission(i);//重新申请权限
+                        return;
+                    } else {
+                        showMissingPermissionDialog();//已经禁止
+                    }
+                }
+            }
+        }
+    }
+
+    // 显示缺失权限提示
+    private void showMissingPermissionDialog() {
+        android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.help);
+        builder.setMessage(R.string.string_help_text);
+
+        // 拒绝, 退出应用
+        builder.setNegativeButton(R.string.quit, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.setPositiveButton(R.string.settings, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startAppSettings();
+            }
+        });
+
+        builder.setCancelable(false);
+
+        builder.show();
+    }
+
+    // 启动应用的设置
+    private void startAppSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse(PACKAGE_URL_SCHEME + getPackageName()));
+        startActivity(intent);
+    }
+
+    private void judgePermission(int i) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{mPermissionList.get(i)}, PERMISSION_REQUEST_CODE);
+    }
+
+    public void toast(String msg) {
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+}
