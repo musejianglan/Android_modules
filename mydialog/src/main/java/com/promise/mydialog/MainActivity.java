e3ad13644c875cd45c3a6f8bfcf6178c9c921915
liulei <musejianglan@163.com>
2018-03-29 16:39:31 +0800

    dialog

---
+++ mydialog/src/main/java/com/promise/mydialog/MainActivity.java
@@ -0,0 +1,22 @@
+package com.promise.mydialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.dialog_fragment})
+    public void dialogFragment(){
+
+    }
+}
