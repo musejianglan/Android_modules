734b43eedf5766e008bf582befa294867beaafb0
liulei <musejianglan@163.com>
2018-05-02 13:57:51 +0800

    mvvm

--- mvvmdemo/src/main/java/com/promise/mvvmdemo/view/MainActivity.java
+++ mvvmdemo/src/main/java/com/promise/mvvmdemo/view/MainActivity.java
@@ -1,15 +1,21 @@
 package com.promise.mvvmdemo.view;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.promise.mvvmdemo.R;
+import com.promise.mvvmdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
+
+
+
 }
