c93ae1cd7b93c27c5ff0e6911fbb586ddf328667
liulei <musejianglan@163.com>
2018-05-03 14:52:40 +0800

    rxjava

--- app/src/main/java/com/musejianglan/promise/MainActivity.java
+++ app/src/main/java/com/musejianglan/promise/MainActivity.java
@@ -11,26 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) {
-            // todo
-        }
     }
 
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //outState.putInt();
-    }
-
-    /**
-     * 保活功能
-     * author:liulei
-     *
-     * created at 2018/4/16 13:47
-     */
-    public void onKkeepLive(View view) {
-
-
-    }
 }
