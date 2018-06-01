c337512696ebeda9c4cf544549853df146bb4a71
liulei <musejianglan@163.com>
2018-06-01 13:01:13 +0800

    dialogfragment

--- mydialog/src/main/java/com/promise/mydialog/MyDialogFragment.java
+++ mydialog/src/main/java/com/promise/mydialog/MyDialogFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +30,12 @@ public class MyDialogFragment extends DialogFragment {
         return super.onCreateDialog(savedInstanceState);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, android.R.style.Theme_Holo_Light_Dialog_MinWidth);//使用默认的AlertDialog样式，宽度充满屏幕，左右保留一些外边距
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -44,6 +51,25 @@ public class MyDialogFragment extends DialogFragment {
         super.onViewCreated(view, savedInstanceState);
 
 
+    }
+
+    /**
+     * 从代码可以看出，首先计算屏幕的宽度，然后将宽度的75%设置到对话框的Window上，高度为WRAP_CONTENT。
+     * 记得要在onCreateView方法中加上
+     * getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+//        Dialog dialog = getDialog();
+//        if (dialog != null) {
+//            DisplayMetrics dm = new DisplayMetrics();
+//            getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+//            Window window = dialog.getWindow();
+//            window.setBackgroundDrawableResource(android.R.color.transparent);
+//            window.setLayout((int) (dm.widthPixels * 0.75), ViewGroup.LayoutParams.WRAP_CONTENT);
+//
+//        }
     }
 
     @OnClick(R.id.sure)
